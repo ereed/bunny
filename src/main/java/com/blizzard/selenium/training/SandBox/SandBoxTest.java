8e065ae82973d318a93f19fae1ad359895696d96
Devin Webster <dwebster@blizzard.com>
2014-05-29 21:19:26 -0700

    initial template

--- src/main/java/com/blizzard/selenium/training/SandBox/SandBoxTest.java
+++ src/main/java/com/blizzard/selenium/training/SandBox/SandBoxTest.java
@@ -7,10 +7,22 @@ package com.blizzard.selenium.training.SandBox;
  * http://docs.seleniumhq.org/docs/07_selenium_grid.jsp
  * https://groups.google.com/forum/#!forum/selenium-users
  */
-public class SandBoxTest
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class SandBoxTest {
+
+    public static void main( String[] args ) {
+
+        // Create Web Driver Instance.
+
+        // Navigate to shop home page.
+
+        // locate the World of Warcraft products link and click it.
+
+        // Click the very first product on the page. "World of Warcraft: Warlords of Draenor".
+
+        // Locate and
+
+        // Locate the active breadcrumb element and assign it to a variable.
+
+        // Verify the active breadcrumb element is "Warlords
     }
 }
