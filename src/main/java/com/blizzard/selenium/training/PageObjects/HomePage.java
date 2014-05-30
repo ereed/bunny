772c33db481f62cf98e3f5f0bb097dac4a8c3695
memlighter <ragnaros@gmail.com>
2014-05-29 23:06:23 -0700

    Commenting on Pageobject - HomePage.class with instructions

--- src/main/java/com/blizzard/selenium/training/PageObjects/HomePage.java
+++ src/main/java/com/blizzard/selenium/training/PageObjects/HomePage.java
@@ -2,6 +2,21 @@ package com.blizzard.selenium.training.PageObjects;
 
 /**
  * Created by DWebster on 5/29/2014.
+ * Summary: Using this type of java class is very useful to breakdown clutter in code.
+ *          Within this java class, only objects are held here. In Selenium's case, it will
+ *          hold temporary values for elements which will be used by another class. This makes
+ *          it possible for the objects to be reused multiple times from other classes.
+ *          Less rewriting the code, the better.
  */
+
 public class HomePage {
+    /**First create an object to retrieve the data you want**/
+    //@FindBy(element locator)
+    //private Type-of-Object name-for-the-object;
+
+
+    /**Once locator object is created, create a getter and setter.
+       Getter and Setters are where the object gets the data and where it can be assigned to.
+    **/
+
 }
