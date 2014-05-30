2f21f44b5f65f53bebd06c9635d1794b981eafe8
DTritan <tritaniv@gmail.com>
2014-05-29 23:32:48 -0700

    junit simple example

--- src/test/java/com/blizzard/selenium/training/TestOne.java
+++ src/test/java/com/blizzard/selenium/training/TestOne.java
@@ -1,7 +1,30 @@
 package com.blizzard.selenium.training;
 
+import com.blizzard.selenium.training.PageObjects.BrowsePage;
+import com.blizzard.selenium.training.PageObjects.HomePage;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
 /**
  * Created by DWebster on 5/29/2014.
  */
 public class TestOne {
+
+    private WebDriver driver;
+    private BrowsePage browsePage;
+    private HomePage homePage;
+
+    public TestOne(final WebDriver driver) {
+        this.driver = new FirefoxDriver();
+        PageFactory.initElements(driver, browsePage);
+        PageFactory.initElements(driver, homePage);
+    }
+
+    @Test
+    public void testStuff(){
+        // perform some sort of test
+        // Use Assert.assertTrue(Expression) to verify something.
+    }
 }
